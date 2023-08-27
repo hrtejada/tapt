c7b78f00a536fcb7f7e56c47f38df879a7b0e381
Ralph Woiwode <rawoiwode@gmail.com>
2023-08-26 20:48:36 -0500

    Adding Typescript to project because I forgot :-/; Creating base Home Screen

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <HomeScreen />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
