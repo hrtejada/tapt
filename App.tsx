4faf6ace5307e4328be49dc189b24683353b058d
Ralph Woiwode <rawoiwode@gmail.com>
2023-08-27 09:25:25 -0500

    Adding React Navigation package

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import HomeScreen from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <HomeScreen />
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </>
   );
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,8 +1,10 @@
 import { StyleSheet, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
+  const insets = useSafeAreaInsets();
   return (
-    <View>
+    <View style={[{ paddingTop: insets.top }]}>
       <Text>HomeScreen</Text>
     </View>
   );
@@ -10,4 +12,6 @@ const HomeScreen = () => {
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+});
