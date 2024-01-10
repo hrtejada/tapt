42cbacae4a81d6d2911dcb40bebcbf193ee1d1ed
Ralph Woiwode <rawoiwode@gmail.com>
2024-01-10 13:40:17 -0600

    Messing around with loading indicators for future calls

--- screens/ImageScreen.tsx
+++ screens/ImageScreen.tsx
@@ -1,5 +1,11 @@
-import { useState } from "react";
-import { ActivityIndicator, Image, StyleSheet, Text, View } from "react-native";
+import { useRef, useState } from "react";
+import {
+  ActivityIndicator,
+  Animated,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { GlobalStyles } from "../constants/styles";
 import { ImageStackProps } from "../util/react-navigation";
 
@@ -13,15 +19,31 @@ import { ImageStackProps } from "../util/react-navigation";
  * TODO: Look into why loading stays active while image is on screen e.g. Zoro in test data
  *
  * @component
- * @version 0.2.0
+ * @version 0.3.0
  * @author  Ralph Woiwode <https://github.com/RAWoiwode>
  */
 const ImageScreen = ({ route }: ImageStackProps) => {
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const fadeAnimationLoader = useRef(new Animated.Value(1)).current;
+  const fadeAnimationImage = useRef(new Animated.Value(0)).current;
 
   const image = route.params.image;
 
+  setTimeout(() => {}, 1000);
+  const onImageLoad = () => {
+    setIsLoading(false);
+    Animated.timing(fadeAnimationLoader, {
+      toValue: 0,
+      duration: 600,
+      useNativeDriver: true,
+    }).start();
+    Animated.timing(fadeAnimationImage, {
+      toValue: 1,
+      duration: 600,
+      useNativeDriver: true,
+    }).start();
+  };
   if (!image) {
     return <Text style={styles.message}>No Image provided</Text>;
   }
@@ -29,18 +51,28 @@ const ImageScreen = ({ route }: ImageStackProps) => {
   return (
     <View style={styles.container}>
       {isLoading && (
-        <ActivityIndicator
-          size={"large"}
-          style={{ flex: 1 }}
-          color={GlobalStyles.colors.accent500}
-        />
+        <Animated.View
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              flex: 1,
+              opacity: fadeAnimationLoader,
+              justifyContent: "center",
+            },
+          ]}
+        >
+          <ActivityIndicator
+            size={"large"}
+            color={GlobalStyles.colors.primary300}
+          />
+        </Animated.View>
       )}
-      <Image
-        style={styles.image}
+      <Animated.Image
+        style={[styles.image, { opacity: fadeAnimationImage }]}
         source={{ uri: image }}
         resizeMode="contain"
         onLoadStart={() => setIsLoading(true)}
-        onLoadEnd={() => setIsLoading(false)}
+        onLoadEnd={onImageLoad}
         onError={() => setError(true)}
       />
       {error && <Text style={styles.message}>Failed to load image</Text>}
@@ -59,7 +91,6 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     width: "100%",
-    backgroundColor: GlobalStyles.colors.accent300,
   },
   message: {
     fontSize: 32,
